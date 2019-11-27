4685c0d4031248858eccb579b9a783236e39f956
HenokZegeye <henokzegeye1990@gmail.com>
2019-11-27 15:15:35 +0300

    separate step-actions in to a method

--- src/components/Forms/ApplicationForm.js
+++ src/components/Forms/ApplicationForm.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
-import { Steps, Button, message, Form } from "antd";
+import { Steps, Button, message, Form, Col, Row } from "antd";
+import Logo from "../Logo";
 import ProgramSelection from "./ProgramSelection";
 import ContactInfo from "./ContactInfo";
+const { Step } = Steps;
+
+const steps = [
+  "Select Program",
+  "Upload Document",
+  "Add Contact Information",
+  "Preview Application",
+  "Confirm"
+];
 
 export class Main extends Component {
   state = {
@@ -25,28 +35,66 @@ export class Main extends Component {
     // console.log(current)
     this.setState({ current });
   }
-  render() {
-    let steps = [
-      {
-        title: "ProgramSelection",
-        content: <ProgramSelection form={this.props.form} />
-      },
-      {
-        title: "ContactInfo",
-        content: <ContactInfo form={this.props.form} />
-      }
-    ];
 
+  onChange = current => {
+    console.log("onChange:", current);
+    this.setState({ current });
+  };
+
+  renderSteps = current => {
+    return (
+      <Col>
+        <Steps
+          onChange={this.onChange}
+          direction="vertical"
+          current={current}
+          className="mt-5"
+        >
+          {steps.map(item => (
+            <Step style={{ height: "100px" }} key={item} title={item} />
+          ))}
+        </Steps>
+      </Col>
+    );
+  };
+
+  renderSteps = current => {
+    return (
+      <Col>
+        <Steps
+          onChange={this.onChange}
+          direction="vertical"
+          current={current}
+          className="mt-5"
+        >
+          {steps.map(item => (
+            <Step style={{ height: "100px" }} key={item} title={item} />
+          ))}
+        </Steps>
+      </Col>
+    );
+  };
+
+  renderSelectedComponent = current => {
+    return (
+      <Col>
+        <Steps
+          onChange={this.onChange}
+          direction="vertical"
+          current={current}
+          className="mt-5"
+        >
+          {steps.map(item => (
+            <Step style={{ height: "100px" }} key={item} title={item} />
+          ))}
+        </Steps>
+      </Col>
+    );
+  };
+
+  renderStepActions = () => {
     return (
       <div>
-        {steps.map(({ title, content }, i) => (
-          <div
-            key={title}
-            className={i === this.state.current ? "foo fade-in" : "foo"}
-          >
-            {content}
-          </div>
-        ))}
         <div className="steps-action">
           {this.state.current < steps.length - 1 && (
             <Button type="primary" onClick={() => this.next()}>
@@ -67,6 +115,55 @@ export class Main extends Component {
             </Button>
           )}
         </div>
+      </div>
+    );
+  };
+
+  render() {
+    const { current } = this.state;
+    let steps = [
+      {
+        title: "ProgramSelection",
+        content: <ProgramSelection form={this.props.form} />
+      },
+      {
+        title: "ContactInfo",
+        content: <ContactInfo form={this.props.form} />
+      }
+    ];
+
+    return (
+      <div>
+        <div className="text-center">
+          <Logo />
+          <h3>
+            To Learn More About What We Offer Visit Our{" "}
+            <a href="http://bitscollege.edu.et/" target="_blank">
+              Website
+            </a>
+          </h3>
+        </div>
+        <Row>
+          <div className="container">
+            <h2>Steps to Apply to BITS College</h2>
+            <Col span={7}>
+              <div>{this.renderSteps(current)}</div>
+            </Col>
+            <Col span={12} offset={5}>
+              <div>
+                {steps.map(({ title, content }, i) => (
+                  <div
+                    key={title}
+                    className={i === this.state.current ? "foo fade-in" : "foo"}
+                  >
+                    {content}
+                  </div>
+                ))}
+              </div>
+              {this.renderStepActions()}
+            </Col>
+          </div>
+        </Row>
       </div>
     );
   }
